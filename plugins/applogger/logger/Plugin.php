2b9d37a1a78cbb6b6458ca79cb0b16eb7dfe63f1
tommy8699 <tomashresko700@gmail.com>
2025-06-05 11:11:07 +0200

    Change of structure for controllers.

--- plugins/applogger/logger/Plugin.php
+++ plugins/applogger/logger/Plugin.php
@@ -2,6 +2,7 @@
 
 namespace AppLogger\Logger;
 
+use Backend\Facades\Backend;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -16,4 +17,35 @@ class Plugin extends PluginBase
         ];
     }
 
+    public function registerNavigation()
+    {
+        return [
+            'logger' => [
+                'label'       => 'Logger',
+                'url'         => Backend::url('applogger/logger/logs'),
+                'icon'        => 'icon-bug',
+                'permissions' => ['applogger.logger.*'],
+                'order'       => 500,
+
+                'sideMenu' => [
+                    'logs' => [
+                        'label'       => 'Logs',
+                        'icon'        => 'icon-list',
+                        'url'         => Backend::url('applogger/logger/logs'),
+                        'permissions' => ['applogger.logger.access_logs'],
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function registerPermissions()
+    {
+        return [
+            'applogger.logger.access_logs' => [
+                'tab'   => 'Logger',
+                'label' => 'Access Logs'
+            ],
+        ];
+    }
 }
