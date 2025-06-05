2db9a0fbc4ec1ce6713ba884c67933d9bb208bdf
tommy8699 <tomashresko700@gmail.com>
2025-06-05 10:25:03 +0200

    Cleaning up unnecessary routes.

--- plugins/applogger/logger/routes.php
+++ plugins/applogger/logger/routes.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use AppLogger\Logger\Http\Controllers\LogController;
 
-Route::get('api/test', function() {
-    return response()->json(['status' => 'ok']);
-});
-
 Route::prefix('api/v1')->group(function () {
     Route::get('logs', [LogController::class, 'index']);
     Route::get('logs/{name}', [LogController::class, 'showByName']);
